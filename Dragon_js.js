adac5a2d3cc12b1a5f873739ddb3f97400e497f7
Umar Kocer <uxkocer@gmail.com>
2022-09-30 11:36:59 -0500

    Add files via upload

---
+++ Dragon_js.js
@@ -0,0 +1,12 @@
+{
+    "manifest_version": 2,
+        "name": Dragon Launcher
+    "description": "Quick Launch Media"
+    "version": "1.0.0",
+        "icon": { "128": "icon_128.png" },
+    "browser_icon": "icon.png",
+        "default_icon": "icon.png",
+            "default_popup": "popup.html"
+},
+"permissions": ["activeTab"]
+}
